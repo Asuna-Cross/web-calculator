b08e114e26a9f728f318f94a9d98f99628fca1b0
asuna-cross <littlecrystaldragon@gmail.com>
2023-02-19 17:42:47 +0000

    pseudocode added

---
+++ main.js
@@ -0,0 +1,26 @@
+// Declare Variables
+// Variable for output number
+// Variable for input number
+// Variable for operand
+
+// Number Clicked
+// Append input number to Number Clicked
+
+// Operator Clicked
+// Check for content in operand variable
+    // If so, calc
+        // Additional Case Handler for division
+            // Check if either number = "0"
+            // If so, don't calc
+    // Store operator
+    // Move outpt into output number
+    // Clear input number
+    // Display Output
+
+// Sum Clicked
+    // Operate
+    // Output
+    // Clear Variables
+
+// Clear Clicked
+    // Clear Variables
